084d36ea85a3b92b42ab04e3bb7bf5663163c34d
Guillaume <guillaume-valsemey@orange.fr>
2026-06-04 17:59:45 +0200

    Redirect root to front home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/')]
-    public function home(): Response
+    #[Route('/', name: 'app_home_home', methods: ['GET'])]
+    public function home(): RedirectResponse
     {
-        return new Response('Bienvenue sur votre accueil!');
+        return $this->redirect('/app/home.html');
+    }
+
+    #[Route('/app/', name: 'app_front_home', methods: ['GET'])]
+    public function appHome(): RedirectResponse
+    {
+        return $this->redirect('/app/home.html');
     }
 }
